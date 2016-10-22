e4cbdd3326c9c63ef4dca773db362d60aa41841f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-22 08:41:15 +0200

    Bosch production line performance [data]

---
+++ bosch-production-line-performance/data/zip2csv.bat
@@ -0,0 +1,10 @@
+"C:\Program Files\7-Zip\7z.exe"  -ocsv   e    zip\test_categorical.csv.zip    
+"C:\Program Files\7-Zip\7z.exe"  -ocsv   e   zip\train_categorical.csv.zip
+
+"C:\Program Files\7-Zip\7z.exe"  -ocsv   e    zip\test_date.csv.zip
+"C:\Program Files\7-Zip\7z.exe"  -ocsv   e   zip\train_date.csv.zip
+
+"C:\Program Files\7-Zip\7z.exe"  -ocsv   e    zip\test_numeric.csv.zip
+"C:\Program Files\7-Zip\7z.exe"  -ocsv   e   zip\train_numeric.csv.zip
+
+"C:\Program Files\7-Zip\7z.exe"  -ocsv   e   zip\sample_submission.csv.zip
